17aa8e7a5f0d9ef8fd9f5ad80235f48acd1eebe5
sudarshan <sudarshanlnx@gmail.com>
2023-05-09 08:41:24 +0530

    new-change

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "sat-23-feb"
-    key            = "dev/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terralock"
+    bucket  = "sat-23-feb"
+    key     = "dev/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+    #    dynamodb_table = "terralock"
 
   }
 }
